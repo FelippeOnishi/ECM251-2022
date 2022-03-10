7a4dd235be68fe2abd38becbad794073c1fc90a5
FelippeOnishi <felippe.onishi@gmail.com>
2022-03-10 13:16:39 -0300

    Ajuste repos

---
+++ Aula 4/intro-poo/src/Caneta.java
@@ -0,0 +1,14 @@
+public class Caneta{
+    //Caracteristicas da Caneta - seus Atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+
+
+    //Comportamentos das canetas - seus Métodos
+    void escrever(String texto){
+        System.out.println(texto);
+    }
+
+}
