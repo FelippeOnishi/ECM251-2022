980fe8c1a20b7def6b17503d8282d6ec89ec5641
FelippeOnishi <Felippe.onishi@gmail.com>
2022-04-27 11:05:10 -0300

    CorrecaoSimulado1

--- SimuladoAtividade1Correcao/Atividade1/src/Conta.java
+++ SimuladoAtividade1Correcao/Atividade1/src/Conta.java
@@ -2,21 +2,16 @@ public class Conta {
     private int idConta;
     private double saldo;
     private static int totalContas = 0;
-
-    // Construtor
     public Conta() {
         this.saldo = 0;
         this.idConta = totalContas;
         totalContas++;
     }
-
-    // Depositar
     public boolean depositar(double valor){
         this.saldo += valor;
         return true;
     }
 
-    // Sacar
     public boolean sacar(double valor){
         if(valor > this.saldo)
             return false;
@@ -24,27 +19,21 @@ public class Conta {
         return true;
     }
 
-    // Transferir
     public boolean transferir(double valor, Conta destino){
         if(!sacar(valor))
             return false;
         return destino.depositar(valor);
     }
-
-    // Getter
     public int getIdConta() {
         return idConta;
     }
-
     public double getSaldo() {
         return saldo;
     }
-
-    // toString
     @Override
     public String toString() {
         return "Conta [idConta=" + idConta + ", saldo=" + saldo + "]";
     }
 
-
+    
 }
--- SimuladoAtividade1Correcao/Atividade1/src/Usuario.java
+++ SimuladoAtividade1Correcao/Atividade1/src/Usuario.java
@@ -3,28 +3,18 @@ public class Usuario {
     private String senha;
     private String email;
     private Conta conta;
-
-    // Construtor
     public Usuario(String nome, String senha, String email) {
         this.nome = nome;
         this.senha = senha;
         this.email = email;
         this.conta = new Conta();
     }
-
-
-    // Getters de nome e conta
     public String getNome() {
         return nome;
     }
-
-
-
     public Conta getConta() {
         return conta;
     }
-
-
     @Override
     public String toString() {
         return "Usuario [conta=" + conta + ", email=" + email + ", nome=" + nome + ", senha=" + senha + "]";
