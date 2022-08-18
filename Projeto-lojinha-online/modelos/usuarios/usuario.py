b082f35bc6dd4b0b63f10c3af5f7f68fb759cb83
FelippeOnishi <felippe.onishi@gmail.com>
2022-08-18 12:32:54 -0300

    Implementando perfil na classe usuario

--- Projeto-lojinha-online/modelos/usuarios/usuario.py
+++ Projeto-lojinha-online/modelos/usuarios/usuario.py
@@ -1,8 +1,10 @@
+from perfil import Perfil
 class Usuario:
     def __init__(self, user_name, password, email) -> None:
         self._user_name = user_name
         self._password = password
         self._email = email
+        self._perfil = Perfil(nome = user_name)
 
     def get_user_name(self):
         return self._user_name
@@ -10,3 +12,5 @@ class Usuario:
     def check_password(self, password):
         return password == self._password
     
+    def get_perfil(self):
+        return self._perfil
