a701fed589a0623b2240e6284613f90c2f4120d4
FelippeOnishi <felippe.onishi@gmail.com>
2022-08-25 12:04:00 -0300

    Manipulacao de arquivos

---
+++ Aula 18/manipulacao_arquivos/main.py
@@ -0,0 +1,14 @@
+# Programa que escreve em um arquivo
+
+arquivo = open("dados.txt", "a")
+continuar = True
+
+while continuar:
+    # time é string
+    time = input("Time (vazio para sair):")
+    # toda string vazia é falsa
+    if not time:
+        continuar = False
+        continue
+    arquivo.write(time+ '\n')
+arquivo.close()
