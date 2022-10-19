4df3bb9582d8b1bde0dcbd3c5c85a4af9ab26df3
FelippeOnishi <felippe.onishi@gmail.com>
2022-10-19 11:11:50 -0300

    Finalizando Aula

--- Projeto-sqlite/tests/teste_controllers.py
+++ Projeto-sqlite/tests/teste_controllers.py
@@ -9,7 +9,11 @@ for item in items:
 novo_item = Item("OLA1", "Cooler REDRAGON Vermelho", 19.90)
 print(controller.inserir_item(novo_item))
 
-print("**************************************")
+print("**************************************************************************************")
 items = controller.pegar_todos_itens()
 for item in items:
     print(item)
+
+print("**************************************************************************************")
+item = controller.pegar_item("CAF")
+print(item)
