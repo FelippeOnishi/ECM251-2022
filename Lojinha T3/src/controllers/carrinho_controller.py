aa52bc0a0da896c2a82dc0aa9bb6eb9b27d83615
FelippeOnishi <felippe.onishi@gmail.com>
2022-11-15 12:16:41 -0300

    Finalizando

--- Lojinha T3/src/controllers/carrinho_controller.py
+++ Lojinha T3/src/controllers/carrinho_controller.py
@@ -7,12 +7,12 @@ class CarrinhoController():
     def get_carrinho(self):
         return self._carrinho
 
-    def add_produto(self,Produto,quantity):
+    def add_produto(self,Produto,quantidade):
         for i in range(len(self.get_carrinho().get_produto())):
             if self._carrinho.get_produto()[i][0].get_name() == Produto.get_name():
-                self._carrinho.get_produto()[i][1] += quantity
+                self._carrinho.get_produto()[i][1] += quantidade
                 return
-        self._carrinho._produto.append([Produto,quantity])
+        self._carrinho._produto.append([Produto,quantidade])
 
     def get_preco_total(self):
         total = 0
